9d8d7171744af2a58b573b6f9574308a0972c8a6
steammilk <candycat202@gmail.com>
2014-09-27 09:55:51 +0900

    gameprogressThread wrtiestory에 이야기 추가
    player
    포션, attack 코드추가
    
    chatserver
    일부 삭제(위 채팅관련중복)

--- MudGameProj/src/Chat/server/GameProgressThread.java
+++ MudGameProj/src/Chat/server/GameProgressThread.java
@@ -58,8 +58,38 @@ public class GameProgressThread extends Thread {
 	}
 	
 	public void writeStory() {
-		
+		ChatServer.sendMessageToAll("강남 한복판.");
+		ChatServer.sendMessageToAll("혼잡한 시내를 뚫고 속속들이 용사들이 여기에 모이고 있다...");
+		ChatServer.sendMessageToAll("세계의 안녕★을 위해 뜻을 품은 자들이라면 그 누구라도 모여 그 뜻을 같이 하자는 방이 내걸렸기 때문이다.");
+		ChatServer.sendMessageToAll("하지만 압박스러운 면접이 모여든 용사들을 기다리고 있었고");
+		ChatServer.sendMessageToAll("그들은 ~뜻밖의 만남~을 극복하고 새로운 세계와 조우했다.");
+		ChatServer.sendMessageToAll("Hello World!");
+		ChatServer.sendMessageToAll("이제 개발자의 꿈을 갖고 있는 용사들이 스펙초월 멘토스쿨에 입학하였다. 그러나 개발자로 가는 길은 만만치 않은데....");
+
 	}
+	/**	public void fightMonsterMessage() {
+	ChatServer.sendMessageToAll("자 이제 새로운 마음으로 시작해보자~~~~~");
+	ChatServer.sendMessageToAll("슬슬 지쳐가기 시작한다..... 아직 많이 남았는데....");
+	ChatServer.sendMessageToAll("이제 게시판 정도는 만들 수(?) 있다.. 하지만 여기서 끝이 아니었다...!");
+	ChatServer.sendMessageToAll("이제 프로젝트 출품을 할 차례이다. 발표가 시작된다.");
+	ChatServer.sendMessageToAll("프로젝트 발표를 마치고 이제 각자의 길로....");
+	ChatServer.sendMessageToAll("어두컴컴한 지하. 낡아보이는 의자와 책상들이 무질서하게 놓여있다. 환기되지 않는 끈적한 공기가 기분 나쁘다."); //지하1층
+	ChatServer.sendMessageToAll("집중적인 교육을 받을 수 있는곳. 레벨업이 빠를 것 같지만 누구도 졸음은 피할 수 없는듯 하다.");  // 3층 강의실
+	ChatServer.sendMessageToAll("넓은 원탁 주변으로 20명 정도 앉을 수 있는 장소. 처음 왔을 때의 그 긴장감이 살아나는 것 같다.");  // 원탁회의장, PK룸
+	ChatServer.sendMessageToAll("적 등장! Java가 나타났다. 객체를 이해한다면 어렵지 않게 물리칠 수 있을것이다.");
+	ChatServer.sendMessageToAll("적 등장! 수 많은 쿼리로 몸을 둘러싼 Oracle이 길을 막아섰다.");
+	ChatServer.sendMessageToAll("적 등장! Network의 등장. Socket부터 차례로 공략하자.");
+	ChatServer.sendMessageToAll("적 등장! JSP가 나타났다!! 앞에 버티고 서 있는 Script부터 무너뜨려야 할 듯 하다.");
+	ChatServer.sendMessageToAll("적 등장! XML가 나타났다!  Parsing으로 공략한다면 반드시 이길 수 있다.");
+	ChatServer.sendMessageToAll("적 등장! Ajax이다. 당황하지 말고...당황했다.");
+	ChatServer.sendMessageToAll("적 등장! Struts가 말했다. 너... 나 알지?");
+	ChatServer.sendMessageToAll("적 등장! 우리에게도 봄은 오는가??? 오긴왔다 Spring이 나타났다");
+	ChatServer.sendMessageToAll("적 등장! CHJ...잘 보이지 않던 최종보스가 나타났다! 여기까지 온 그 강인함과 지혜로 돌파해보자.");
+	ChatServer.sendMessageToAll("훌륭한 초보 개발자로 전직한 플레이어들. 하지만 세상은 1위만 기억하는법! 동료들과의 우열을 가려보자."); // 토너먼트, PK룸
+	ChatServer.sendMessageToAll("적 등장!일것같냐!!!아니거든?아니거든?적아니거든???");
+	}
+*/		
+	
 	
 	public void fightMonster() {
 		
--- MudGameProj/src/Chat/server/Player.java
+++ MudGameProj/src/Chat/server/Player.java
@@ -11,12 +11,30 @@ public class Player extends Unit {
 	}
 	public void useItem(){
 		//포션 사용
+		for(int i = 0; i < ChatServer.players.size(); i++){
+			Player p = ChatServer.players.get(i);
+			p.hp += 100;
+			p.potionCount = potionCount - 1;
+		}
 	}
 	public void useSkill(){
 		//스킬 사용
 	}
 	public void attack(){
 		//일반 공격
+		//플레이어가 30~40 사이의 랜덤 데미지를 받는 코드
+		/*	for(int i = 0; i < ChatServer.players.size(); i++){ 
+		Player p = ChatServer.players.get(i);
+		
+		//0~10 사이의 1개의 정수인 난수 발생
+		Random r = new Random(System.currentTimeMillis());
+		  for (int i = 0; i < 2; i++) {
+		       // nextInt 리턴값은 0 <= val < 11 로 나옵니다.
+		       System.out.println(r.nextInt(11)+1); 
+		  }
+		  
+		p.hp -= 30 + r ;
+		}*/
 	}
 	public void setName(String name){
 		this.name = name;
