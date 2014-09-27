c94def73612375556f15727d282f77ed5f02b3f2
bit <bit@bit-PC>
2014-09-27 14:39:27 +0900

    Test 추가

---
+++ MudGameProj/src/Test/Consumer.java
@@ -0,0 +1,28 @@
+package Test;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer extends Thread {
+private BlockingQueue queue;
+	
+	public Consumer(BlockingQueue queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+				
+				String msg = (String)queue.take();
+				
+				System.out.println("¸Þ½ÃÁö¸¦ ²¨³À´Ï´Ù. : " + msg + "[" + queue.size() + "]");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+	}	
+}
---
+++ MudGameProj/src/Test/Starter.java
@@ -0,0 +1,21 @@
+package Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		
+		BlockingQueue queue = new ArrayBlockingQueue(50);
+		//BlockingQueue queue = new LinkedBlockingQueue();
+		Thread p = new Thread(new Producer(queue));
+		Thread c = new Thread(new Consumer(queue));
+		
+		p.start();
+		c.start();
+		
+	}
+	
+}
