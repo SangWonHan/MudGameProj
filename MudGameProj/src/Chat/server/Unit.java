eea5245767dedbad527c627c9596a1f576132dbd
bit <bit@bit-PC>
2014-09-27 10:05:40 +0900

    Unit, Player 클래스 수정

--- MudGameProj/src/Chat/server/Unit.java
+++ MudGameProj/src/Chat/server/Unit.java
@@ -5,4 +5,19 @@ public class Unit {
 	int minDamage;
 	int maxDamage;
 	String name;
+	
+	public Unit() {
+		
+	}
+	
+	public void attack(Unit unit){
+		//일반 공격
+	}
+	
+	public Unit(int hp, int minDamage, int maxDamage, String name) {
+		this.hp = hp;
+		this.minDamage = minDamage;
+		this.maxDamage = maxDamage;
+		this.name = name;
+	}
 }
